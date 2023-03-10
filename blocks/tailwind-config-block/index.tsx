304c8ca3439fb54dd04822dc7dcacb43e019c316
BrentWMiller <brentwmiller93@gmail.com>
2023-03-10 09:27:06 -0500

    💄 adds WIP label

--- blocks/tailwind-config-block/index.tsx
+++ blocks/tailwind-config-block/index.tsx
@@ -1,20 +1,26 @@
 import resolveConfig from 'tailwindcss/resolveConfig';
 import { FileBlockProps } from '@githubnext/blocks';
-import { Box, Heading } from '@primer/react';
+import { Box, Heading, Label } from '@primer/react';
 import './index.css';
 import { Config } from 'tailwindcss';
 import ColorPalette from './components/ColorPalette';
 import FontSizes from './components/FontSizes';
+import { ThemeConfig } from 'tailwindcss/types/config';
 
 export default function ExampleFileBlock(props: FileBlockProps) {
-  const { context, content, metadata, onUpdateMetadata } = props;
+  const { content } = props;
 
   // Tailwind theme config
+  let theme: Partial<ThemeConfig & { extend: Partial<ThemeConfig> }> | undefined = {};
   const fullConfig = resolveConfig(content as any);
-  const theme = fullConfig.theme as Config['theme'];
+  theme = fullConfig.theme as Config['theme'];
 
   return (
     <Box p={4}>
+      <Box pb={4}>
+        <Label variant='attention'>Work in Progress</Label>
+      </Box>
+
       <Box borderColor='border.default' borderWidth={1} borderStyle='solid' borderRadius={6} overflow='hidden' p={4} mb={4}>
         <Heading sx={{ mb: 6 }}>Colors</Heading>
         {theme?.colors && <ColorPalette colors={theme.colors} />}
